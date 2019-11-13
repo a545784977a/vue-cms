d3c3442bdfb8b90c3e0ef73c4eacfa2c237bd813
LiuQ-Q <545784977@qq.com>
2019-11-13 23:13:14 +0800

    FIXED

--- src/components/subcomponents/comment.vue
+++ src/components/subcomponents/comment.vue
@@ -73,7 +73,6 @@
         })
       }
     },
-
     props: ["id"]
   }
 </script>
--- src/components/subcomponents/shopcar_numbox.vue
+++ src/components/subcomponents/shopcar_numbox.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="mui-numbox" data-numbox-min='1' style="height:25px;width:110px;">
-    <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-    <input id="test" class="mui-input-numbox" type="number" :value="initcount" @change="countChanged" ref="numbox" readonly />
-    <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
-  </div>
+	<div class="mui-numbox" data-numbox-min='1' style="height:25px;width:110px;">
+		<button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+		<input id="test" class="mui-input-numbox" type="number" :value="initcount" @change="countChanged" ref="numbox" readonly />
+		<button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+	</div>
 </template>
 
 <script>
-  import mui from '../../lib/mui/js/mui.min.js'
+	import mui from '../../lib/mui/js/mui.min.js'
 
-  export default {
-    mounted () {
-      mui(".mui-numbox").numbox();
-    },
-    methods: {
-      countChanged () {
-        this.$store.commit("updateGoodsInfo", {
-          id: this.goodsId,
-          count: this.$refs.numbox.value
-        })
-      }
-    },
-    props: ["initcount", "goodsId"]
-  }
+	export default {
+		mounted () {
+			mui(".mui-numbox").numbox();
+		},
+		methods: {
+			countChanged () {
+				this.$store.commit("updateGoodsInfo", {
+					id: this.goodsId,
+					count: this.$refs.numbox.value
+				})
+			}
+		},
+		props: ["initcount", "goodsId"]
+	}
 </script>
 
 <style lang="scss" scoped>
-  
+	
 </style>
