11b1aabbcc68ce8b72f1ef87fdef6127c5bab5a8
LiuQ-Q <545784977@qq.com>
2019-11-15 23:39:42 +0800

    space to tab

--- src/components/subcomponents/comment.vue
+++ src/components/subcomponents/comment.vue
@@ -1,105 +1,105 @@
 <template>
-  <div class="cmt-container">
-    <h3>发表评论</h3>
-    <hr>
-    <textarea placeholder="请输入内容(最多120字)" maxlength="120" v-model="msg"></textarea>
+	<div class="cmt-container">
+		<h3>发表评论</h3>
+		<hr>
+		<textarea placeholder="请输入内容(最多120字)" maxlength="120" v-model="msg"></textarea>
 
-    <mt-button type="primary" size="large" @click="postComment">发表评论</mt-button>
+		<mt-button type="primary" size="large" @click="postComment">发表评论</mt-button>
 
-    <div class="cmt-list">
-      <div class="cmt-item" v-for="(item, i) in comments" :key="item.id">
-        <div class="cmt-title">
-          第{{ i+1 }}楼&nbsp;&nbsp;用户:{{ item.user_name }}&nbsp;&nbsp;发表时间:{{ item.add_time | dateFormat }}
-        </div>
-        <div class="cmt-body">
-         {{ item.content === 'undefined' ? '此用户很懒,什么都没说' : item.content }}
-        </div>
-      </div>
-    </div>
+		<div class="cmt-list">
+			<div class="cmt-item" v-for="(item, i) in comments" :key="item.id">
+				<div class="cmt-title">
+					第{{ i+1 }}楼&nbsp;&nbsp;用户:{{ item.user_name }}&nbsp;&nbsp;发表时间:{{ item.add_time | dateFormat }}
+				</div>
+				<div class="cmt-body">
+				 {{ item.content === 'undefined' ? '此用户很懒,什么都没说' : item.content }}
+				</div>
+			</div>
+		</div>
 
-    <mt-button type="danger" size="large" plain @click="getMore">加载更多</mt-button>
-  </div>
+		<mt-button type="danger" size="large" plain @click="getMore">加载更多</mt-button>
+	</div>
 </template>
 
 <script>
-  import { Toast } from 'mint-ui';
-  export default {
-    data () {
-      return {
-        pageIndex: 1,
-        comments: [],
-        msg: ''
-      }
-    },
+	import { Toast } from 'mint-ui';
+	export default {
+		data () {
+			return {
+				pageIndex: 1,
+				comments: [],
+				msg: ''
+			}
+		},
 
-    created () {
-      this.getComments();
-    },
+		created () {
+			this.getComments();
+		},
 
-    methods: {
-      getComments () {
-        this.$http.get("api/getcomments/"+ this.id +"?pageindex=" + this.pageIndex).then(result => {
-          if (result.body.status === 0) {
-            this.comments = this.comments.concat(result.body.message);
-          } else {
-            Toast('评论获取失败')
-            console.log('失败id' + this.id)
-          }
-        });
-      },
-      getMore () {
-        this.pageIndex++;
-        this.getComments();
-      },
-      postComment () {
-        if (this.msg.trim().length === 0) {
-          return Toast('评论不能为空...')
-        }
+		methods: {
+			getComments () {
+				this.$http.get("api/getcomments/"+ this.id +"?pageindex=" + this.pageIndex).then(result => {
+					if (result.body.status === 0) {
+						this.comments = this.comments.concat(result.body.message);
+					} else {
+						Toast('评论获取失败')
+						console.log('失败id' + this.id)
+					}
+				});
+			},
+			getMore () {
+				this.pageIndex++;
+				this.getComments();
+			},
+			postComment () {
+				if (this.msg.trim().length === 0) {
+					return Toast('评论不能为空...')
+				}
 
-        this.$http.post('api/postcomment/' + this.$route.params.id, {
-          content: this.msg.trim()
-        }).then(function (result) {
-          if (result.body.status === 0) {
-            var cmt = {
-              user_name: '匿名用户',
-              add_time: Date.now(),
-              content: this.msg.trim()
-            }
-            this.comments.unshift(cmt)
-            this.msg = ''
-          } else {
-            Toast('评论失败')
-          }
-        })
-      }
-    },
-    props: ["id"]
-  }
+				this.$http.post('api/postcomment/' + this.$route.params.id, {
+					content: this.msg.trim()
+				}).then(function (result) {
+					if (result.body.status === 0) {
+						var cmt = {
+							user_name: '匿名用户',
+							add_time: Date.now(),
+							content: this.msg.trim()
+						}
+						this.comments.unshift(cmt)
+						this.msg = ''
+					} else {
+						Toast('评论失败')
+					}
+				})
+			}
+		},
+		props: ["id"]
+	}
 </script>
 
 <style lang="scss" scoped>
-  .cmt-container {
-    h3 {
-      font-size: 18px;
-    }
-    textarea {
-      font-size: 14px;
-      height: 85px;
-    }
+	.cmt-container {
+		h3 {
+			font-size: 18px;
+		}
+		textarea {
+			font-size: 14px;
+			height: 85px;
+		}
 
-    .cmt-list {
-      margin: 5px 0;
-      .cmt-item {
-        font-size: 13px;
-        .cmt-title {
-          line-height: 30px;
-          background-color: #ccc;
-        }
-        .cmt-body {
-          line-height: 35px;
-          text-indent: 2em;
-        }
-      }
-    }
-  }
+		.cmt-list {
+			margin: 5px 0;
+			.cmt-item {
+				font-size: 13px;
+				.cmt-title {
+					line-height: 30px;
+					background-color: #ccc;
+				}
+				.cmt-body {
+					line-height: 35px;
+					text-indent: 2em;
+				}
+			}
+		}
+	}
 </style>
