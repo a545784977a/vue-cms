2995be6e20fb2cadd356d23527ad570c9f4ea875
LiuQ-Q <545784977@qq.com>
2020-10-18 20:00:09 +0800

    practice

--- src/components/news/NewsInfo.vue
+++ src/components/news/NewsInfo.vue
@@ -1,67 +1,68 @@
 <template>
-  <div class="newsinfo-container">
-    <h3 class="title">{{ newsInfo.title }}</h3>
-    <p class="subtitle">
-      <span>发表时间:{{ newsInfo.add_time | dateFormat }}</span>
-      <span>点击:{{ newsInfo.click }}次</span>
-    </p>
+<div class="newsinfo-container">
+  <h3 class="title">{{ newsInfo.title }}</h3>
+  <p class="subtitle">
+    <span>发表时间:{{ newsInfo.add_time | dateFormat }}</span>
+    <span>点击:{{ newsInfo.click }}次</span>
+  </p>
 
-    <hr>
+  <hr>
+  <div class="content" v-html="newsInfo.content"></div>
 
-    <div class="content" v-html="newsInfo.content"></div>
-
-    <comment-box :id="this.id"></comment-box>
-  </div>
+  <comment-box :id="this.id"></comment-box>
+</div>
 </template>
 
 <script>
-  import { Toast } from 'mint-ui';
-  import comment from '../subcomponents/comment.vue'
+import {
+  Toast
+} from 'mint-ui';
+import comment from '../subcomponents/comment.vue'
 
-  export default {
-    data () {
-      return {
-        id: this.$route.params.id,
-        newsInfo: {}
-      }
-    },
-    created () {
-      this.getNewsInfo();
-    },
-    methods: {
-      getNewsInfo () {
-        this.$http.get('api/getnew/' + this.id).then(result => {
-          if (result.body.status === 0) {
-            this.newsInfo = result.body.message[0];
-          } else {
-            Toast('新闻内容获取失败')
-            console.log('失败id' + this.id)
-          }
-        });
-      },
-    },
-    components: {
-      'comment-box': comment
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      newsInfo: {}
     }
+  },
+  created() {
+    this.getNewsInfo();
+  },
+  methods: {
+    getNewsInfo() {
+      this.$http.get('api/getnew/' + this.id).then(result => {
+        if (result.body.status === 0) {
+          this.newsInfo = result.body.message[0];
+        } else {
+          Toast('新闻内容获取失败')
+          console.log('失败id' + this.id)
+        }
+      });
+    },
+  },
+  components: {
+    'comment-box': comment
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  .newsinfo-container {
-    padding: 0 4px;
+.newsinfo-container {
+  padding: 0 4px;
 
-    .title {
-      font-size: 16pz;
-      text-align: center;
-      margin: 15px 0;
-      color: red;
-    }
-
-    .subtitle {
-      font-size: 13px;
-      color: blue;
-      display: flex;
-      justify-content: space-between;
-    }
+  .title {
+    font-size: 16pz;
+    text-align: center;
+    margin: 15px 0;
+    color: red;
   }
+
+  .subtitle {
+    font-size: 13px;
+    color: blue;
+    display: flex;
+    justify-content: space-between;
+  }
+}
 </style>
